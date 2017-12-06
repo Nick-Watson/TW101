e83b2d2514907f8c80ae088ffa05ef65d3ad6fec
Nick-Watson <nickwatson248@gmail.com>
2017-12-06 14:13:57 +0000

    changed output to substring

--- src/com/watson/nick/TWExercises/FizzBuzz.java
+++ src/com/watson/nick/TWExercises/FizzBuzz.java
@@ -4,7 +4,7 @@ public class FizzBuzz {
     public String fizzBuzz() {
         int count = 1;
         String output = "";
-        while (count < 100) {
+        while (count <= 100) {
             if (count % 3 == 0 && count % 5 == 0) output = output + "FizzBuzz\n";
             else if (count % 5 == 0) output = output + "Buzz\n";
             else if (count % 3 == 0) output = output + "Fizz\n";
@@ -13,6 +13,6 @@ public class FizzBuzz {
             count++;
         }
 
-        return output + "Buzz";
+        return output.substring(0, output.length() - 1);
     }
 }
